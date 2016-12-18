a84da2adc75fc35587ba57a9f27947dd3cd5a121
eipporko <me@davidantunez.es>
2016-12-18 20:31:20 +0100

    Foliometro v1.0

---
+++ README.md
@@ -0,0 +1,4 @@
+# Foliometro
+
+## Arduino Schematic
+![Connection Scheme](figures/sketch.png)
--- foliometro.ino
+++ foliometro.ino
@@ -20,31 +20,32 @@
    License along with this library.
 
 */
-#define SENSOR_RESOLUTION 255
-#define WINDOW_RESOLUTION 128
 #define DEBOUNCE_TIME     150
-#define NUM_OF_LEDS       9
+#define NUM_OF_LEDS       11
 #define CONFIRM_DURATION  250
 #define BLINK_DELAY       200
 #define TIMEOUT           1000
 
 //Pins
-#define BUTTON_UP_PIN     0
-#define BUTTON_DOWN_PIN   1 
+#define BUTTON_DOWN_PIN   0 
+#define BUTTON_UP_PIN     1 
 
-#define LED_MINUS4_PIN    2
-#define LED_MINUS3_PIN    3
-#define LED_MINUS2_PIN    4
-#define LED_MINUS1_PIN    5
-#define LED_ZERO_PIN      6
-#define LED_PLUS1_PIN     7
-#define LED_PLUS2_PIN     8
-#define LED_PLUS3_PIN     9
-#define LED_PLUS4_PIN     10
+#define LED_MINUS5_PIN    2
+#define LED_MINUS4_PIN    3
+#define LED_MINUS3_PIN    4
+#define LED_MINUS2_PIN    5
+#define LED_MINUS1_PIN    6
+#define LED_ZERO_PIN      7
+#define LED_PLUS1_PIN     8
+#define LED_PLUS2_PIN     9
+#define LED_PLUS3_PIN     10
+#define LED_PLUS4_PIN     16
+#define LED_PLUS5_PIN     14
 
 #define SENSOR_PIN        18
 
-short ledPins[9] = {
+short ledPins[NUM_OF_LEDS] = {
+  LED_MINUS5_PIN,
   LED_MINUS4_PIN,
   LED_MINUS3_PIN,
   LED_MINUS2_PIN,
@@ -54,10 +55,10 @@ short ledPins[9] = {
   LED_PLUS2_PIN,
   LED_PLUS3_PIN,
   LED_PLUS4_PIN,
+  LED_PLUS5_PIN
 };
 
-short pressure = 0;
-volatile byte level = 4;
+volatile byte level = 5;
 volatile unsigned long lastInterruptTime = 0;
 
 typedef enum ProgramState {MAIN_STATE, CHANGE_LEVEL_STATE} ProgramState_t;
@@ -124,9 +125,18 @@ void confirmAnimation() {
 
 void resetLeds() {
   for (byte i = 0; i < NUM_OF_LEDS; i++){
-    pinMode(ledPins[i], OUTPUT);
     digitalWrite(ledPins[i], LOW);
   }
+}
+
+
+void turnOnLed(short ledIndex) {
+  static byte lastLedIndex = 0;
+  ledIndex = max (ledIndex, 0);
+  ledIndex = min (ledIndex, NUM_OF_LEDS-1); 
+  digitalWrite(ledPins[lastLedIndex], LOW);
+  digitalWrite(ledPins[ledIndex], HIGH);
+  lastLedIndex = ledIndex;
 }
 
 
@@ -137,9 +147,12 @@ void changeStateProgram(ProgramState_t newState) {
 
 
 void mainState() {
-  pressure = analogRead(SENSOR_PIN);    
-  pressure = map(pressure, 0, 1023, 0, SENSOR_RESOLUTION);
-  //Serial.println(pressure);  
+  short pressure = analogRead(SENSOR_PIN); 
+  byte ledIndex = map(pressure, 0, 1023, 0, (NUM_OF_LEDS-1)*2);
+  turnOnLed(ledIndex - level);
+   
+  if (stateProgram != MAIN_STATE)
+    resetLeds();
 }
 
 
@@ -153,13 +166,17 @@ void changeLevelState() {
     confirmAnimation();
     changeStateProgram(MAIN_STATE);
   }
+
+  if (stateProgram != CHANGE_LEVEL_STATE)
+    resetLeds();
 }
 
 
 void setup() {
-  Serial.begin(9600);
-
-  resetLeds();
+  for (byte i = 0; i < NUM_OF_LEDS; i++){
+    pinMode(ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], LOW);
+  }
     
   pinMode(BUTTON_UP_PIN, INPUT);
   pinMode(BUTTON_DOWN_PIN, INPUT);
